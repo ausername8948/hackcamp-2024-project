a6c4ace626b599b2d084dfe2445b19f5299ed2c8
Sam <mastermas2019@gmail.com>
2024-11-10 12:53:04 -0800

    initial reels prototype

---
+++ my-app/src/Card.js
@@ -0,0 +1,136 @@
+import React from 'react';
+import './Card.css';
+import CardFooter from './CardFooter';
+import CardSidebar from './CardSidebar';
+
+
+function Card() {
+    return (
+        <div className="card">
+            <div className="card_header">
+                <h1>This is the card header</h1>
+            </div>
+            <CardFooter />
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+             <CardSidebar />
+        </div>
+    )
+}
+
+export default Card;
---
+++ my-app/src/CardFooter.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./CardFooter.css";
+
+function CardFooter() {
+  return (
+    <div className="cardFooter">
+        <div className="cardFooter_text">
+            <h3>This is the card footer</h3>
+            <p>this is a description</p>
+        </div>
+      
+    </div>
+  );
+}
+
+export default CardFooter;
