1ae444d2af34c897a82f11dd988aa85d92384464
Sam <mastermas2019@gmail.com>
2024-11-10 20:16:12 -0800

    styled most pages

--- my-app/src/components/QuestionModal.js
+++ my-app/src/components/QuestionModal.js
@@ -1,29 +1,40 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './QuestionModal.css';
 
-function QuestionModal({ isOpen, onClose, onSubmit }) {
-  if (!isOpen) return null;
+function QuestionModal({ isOpen, onClose, onSubmit, initialQuestion, initialAnswer }) {
+  const [question, setQuestion] = useState(initialQuestion);
+  const [answer, setAnswer] = useState(initialAnswer);
 
-  let question = '';
-  let answer = '';
+  useEffect(() => {
+    setQuestion(initialQuestion);
+    setAnswer(initialAnswer);
+  }, [initialQuestion, initialAnswer]);
+  
+ 
+
 
   const handleSubmit = () => {
     onSubmit(question, answer);
     onClose();
   };
 
+  if (!isOpen) return null;
+
   return (
     <div className="modal-overlay">
       <div className="modal">
         <h2>New Question</h2>
-        <label>
-          Question:
-          <input type="text" onChange={(e) => (question = e.target.value)} />
-        </label>
-        <label>
-          Answer:
-          <input type="text" onChange={(e) => (answer = e.target.value)} />
-        </label>
+        <input
+          type="text"
+          value={question}
+          onChange={(e) => setQuestion(e.target.value)}
+        />
+        <h2>Answer</h2>
+        <input
+          type="text"
+          value={answer}
+          onChange={(e) => setAnswer(e.target.value)}
+        />
         <button onClick={handleSubmit}>Submit</button>
         <button onClick={onClose}>Close</button>
       </div>
